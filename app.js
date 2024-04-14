773c7241d3e247998d03795f4f81941b296eaf89
Kshitij Kochhar <kshitij@ctfguide.com>
2024-04-14 02:11:39 -0400

    DB set up

--- app.js
+++ app.js
@@ -1,13 +1,50 @@
-const express = require('express');
-const app = express();
-const PORT = 3003; // Use the specified port or default to 3000
+require('dotenv').config();
 
-// Define a route
-app.get('/', (req, res) => {
-    res.send('Hello Express!');
+const express = require('express');
+const mongoose = require('mongoose');
+const User = mongoose.model('User', { name: String, age: Number });
+const app = express();
+const PORT = 3003; 
+const uri = process.env.MONGODB_CONNECTION_STRING;
+const connection = mongoose.connection;
+
+app.use(express.json());
+
+mongoose.connect(uri, {
+    dbName: 'Users',
 });
 
-// Start the server
+connection.once('open', () => {
+    console.log('MongoDB connection established successfully');
+});
+
+app.get('/userList', async (req, res) => {
+    try {
+        const users = await User.find();
+        return res.status(200).json({ users: users });
+    } catch (error) {
+        console.log("error: " + error);
+        return res.status(500).json({ error: error });
+    }
+})
+
+app.post('/user', async (req, res) => {
+    try {
+        console.log("req.body: " + req.body.name + " " + req.body.age);
+        const { name, age } = req.body;
+        const user = new User({ name, age });
+        await user.save();
+        return res.status(200).json({ user: user });
+    } catch (error) {
+        console.log("error: " + error);
+        return res.status(500).json({ error: error });
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello Express!');
 });
