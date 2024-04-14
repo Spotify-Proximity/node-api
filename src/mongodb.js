8f08c59c6bc385c335a96a323532834ebaaee667
Kshitij Kochhar <kshitij@ctfguide.com>
2024-04-14 00:29:33 -0400

    first commit fr

---
+++ src/mongodb.js
@@ -0,0 +1,24 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://kkochhar2004:dirttag0123A!@spotifyproximitycluster.5d53jbo.mongodb.net/?retryWrites=true&w=majority&appName=SpotifyProximityCluster";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
